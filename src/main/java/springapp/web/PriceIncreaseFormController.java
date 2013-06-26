04f8a1e3144dae0d61e245752070b4142f127cba
wjirawong <wjirawong@BDC-BKK10.BDC-BKK>
2013-06-26 09:03:29 +0700

    add priceIncreaseFormController

---
+++ src/main/java/springapp/web/PriceIncreaseFormController.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package springapp.web;
+
+import org.springframework.web.servlet.mvc.SimpleFormController;
+
+/**
+ *
+ * @author wjirawong
+ */
+public class PriceIncreaseFormController extends SimpleFormController{
+    
+}
